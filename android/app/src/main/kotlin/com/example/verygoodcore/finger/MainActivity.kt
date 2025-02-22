b33b97cac0186890d829ca121fee0acebb998f16
bennettchamberlain <chamberlain.bennett@gmail.com>
2025-02-22 07:26:01 -0800

    feat: create vgv template flutter app

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/finger/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.finger
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_production.dart
@@ -0,0 +1,6 @@
+import 'package:finger/app/app.dart';
+import 'package:finger/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
