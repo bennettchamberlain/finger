13238613c2758ba542fe727e808efb38da22e5f5
bennettchamberlain <chamberlain.bennett@gmail.com>
2025-02-22 07:33:06 -0800

    feat: update signing

--- packages/app_core/test/src/app_core_test.dart
+++ packages/app_core/test/src/app_core_test.dart
@@ -1,12 +1,5 @@
 // ignore_for_file: prefer_const_constructors
 
-import 'package:app_core/app_core.dart';
-import 'package:flutter_test/flutter_test.dart';
-
 void main() {
-  group('AppCore', () {
-    test('can be instantiated', () {
-      expect(AppCore(), isNotNull);
-    });
-  });
+
 }
